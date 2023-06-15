6f1ea29f593171d708b55e0b265a07470e7b9b93
Johann Schopplich <mail@johannschopplich.com>
2023-06-15 08:55:10 +0200

    refactor: cleanup button component

--- components/Element/Button.vue
+++ components/Element/Button.vue
@@ -10,11 +10,11 @@ defineProps<{
     :is="href ? 'a' : 'button'"
     :type="!href ? 'button' : undefined"
     :href="href"
-    class="inline-flex items-center justify-center p-3 text-sm font-medium shadow-sm active:translate-y-[1px] disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-500 disabled:hover:bg-gray-200"
+    class="inline-flex items-center justify-center p-3 text-sm font-medium shadow-sm active:translate-y-[1px] disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-500 focus-visible:outline-2 focus-visible:outline-primary-300 focus-visible:outline-offset-2 focus-visible:outline disabled:hover:bg-gray-200"
     :class="[
       theme === 'secondary'
-        ? 'bg-white text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50'
-        : 'bg-primary-300 text-black hover:bg-primary-200 focus-visible:outline-2 focus-visible:outline-primary-300 focus-visible:outline-offset-2 focus-visible:outline',
+        ? 'bg-white text-black ring-1 ring-gray-300 ring-inset hover:bg-gray-50'
+        : 'bg-primary-300 text-black hover:bg-primary-200',
     ]"
   >
     <slot />
