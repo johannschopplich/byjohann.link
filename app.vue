8cf4a53acd30aeb5084fd7aa4f3bee32f2f5cef9
Johann Schopplich <mail@johannschopplich.com>
2024-02-08 14:16:41 +0100

    chore: add missing `favicon.ico`

--- app.vue
+++ app.vue
@@ -2,19 +2,32 @@
 import '~/assets/css/main.css'
 import '~/assets/css/prose.css'
 
-import favicon from '~/assets/favicon.svg?raw'
+// import favicon from '~/assets/favicon.svg?raw'
 
 const { languageCode } = getLocalePreference()
 
-useHead({
+useServerSeoMeta({
+  themeColor: '#fefaf7',
+})
+
+useServerHead({
   htmlAttrs: {
     lang: languageCode === 'de' ? 'de' : 'en',
     class: 'bg-off-white',
   },
   link: [
+    // {
+    //   rel: 'icon',
+    //   href: `data:image/svg+xml,${favicon.replace('"', '%22')}`,
+    // },
     {
       rel: 'icon',
-      href: `data:image/svg+xml,${favicon.replace('"', '%22')}`,
+      href: '/favicon.ico',
+      sizes: '32x32',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/apple-touch-icon.png',
     },
   ],
 })
