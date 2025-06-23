0d8d644bc5d236f378358ecf32ea7e49e198b338
Johann Schopplich <mail@johannschopplich.com>
2025-06-23 07:45:03 +0200

    chore: remove Plausible

--- app/app.vue
+++ app/app.vue
@@ -11,41 +11,35 @@ if (import.meta.dev) {
   useNuxtApp().hooks.hook('vue:error', console.error)
 }
 
-const { siteUrl } = useRuntimeConfig().public
 const { languageCode } = getLocalePreference()
 
-useServerSeoMeta({
+useSeoMeta({
   themeColor: '#fefaf7',
 })
 
-useServerHead({
-  htmlAttrs: {
-    lang: languageCode === 'de' ? 'de' : 'en',
-    class: 'bg-off-white',
-  },
-  link: [
-    // {
-    //   rel: 'icon',
-    //   href: `data:image/svg+xml,${favicon.replace('"', '%22')}`,
-    // },
-    {
-      rel: 'icon',
-      href: '/favicon.ico',
-      sizes: '32x32',
+if (import.meta.server) {
+  useHead({
+    htmlAttrs: {
+      lang: languageCode === 'de' ? 'de' : 'en',
+      class: 'bg-off-white',
     },
-    {
-      rel: 'apple-touch-icon',
-      href: '/apple-touch-icon.png',
-    },
-  ],
-  script: [
-    {
-      src: 'https://plausible.io/js/script.js',
-      'data-domain': new URL(siteUrl).hostname,
-      defer: true,
-    },
-  ],
-})
+    link: [
+      // {
+      //   rel: 'icon',
+      //   href: `data:image/svg+xml,${favicon.replace('"', '%22')}`,
+      // },
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+        sizes: '32x32',
+      },
+      {
+        rel: 'apple-touch-icon',
+        href: '/apple-touch-icon.png',
+      },
+    ],
+  })
+}
 </script>
 
 <template>
